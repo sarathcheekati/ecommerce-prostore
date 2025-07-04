37e9733acb32883c9bb6a88ded4d3ea931100edc
Sarath <cheekatisarath@gmail.com>
2025-07-04 20:35:45 +0530

    fixed errors in db seeds

--- db/seed.ts
+++ db/seed.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from "@/lib/generated/prisma";
-import sampleData from "./sample-data";
+// import { PrismaClient } from "@/lib/generated/prisma";
+// import sampleData from "./sample-data";
 
-async function main() {
-  try {
-    const prisma = new PrismaClient();
-    await prisma.product.deleteMany();
-    await prisma.product.createMany({ data: sampleData?.products });
-    console.log("Database Seeded Successfully");
-  } catch (error) {
-    console.log("Data Seeding error", error);
-  }
-}
-main();
+// async function main() {
+//   try {
+//     const prisma = new PrismaClient();
+//     await prisma.product.deleteMany();
+//     await prisma.product.createMany({ data: sampleData?.products });
+//     console.log("Database Seeded Successfully");
+//   } catch (error) {
+//     console.log("Data Seeding error", error);
+//   }
+// }
+// main();
